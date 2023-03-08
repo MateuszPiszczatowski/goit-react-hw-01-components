26c3e6ba889ab820bcec22a351a86500d3175143
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-03-08 11:58:30 +0100

    Add broken section for user without avatar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import user from "./data/user.json";
+import brokenUser from "./data/brokenUser.json";
 import data from "./data/data.json";
 import friends from "./data/friends.json";
 import transactions from "./data/transactions.json";
@@ -25,6 +26,8 @@ function App() {
           </a>
         </header>*/}
       <Profile {...user} />
+      <div className="line" />
+      <Profile {...brokenUser} />
       <div className="line" />
       <Statistics label="Upload Stats" stats={data} />
       <div className="line" />
